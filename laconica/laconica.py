19977f7a8c9d4bdaf3adb5da03d86e24580c2b64
Daesun Magay <deson_m@icloud.comls>
2023-09-09 20:46:31 +0600

    W.I.P commit for reflex programm
    Centered the input bar and the buttons, added a menu bar with info: includes the name of the program and my github link for now. Still not ideal but progress.

--- laconica/laconica.py
+++ laconica/laconica.py
@@ -23,31 +23,43 @@ class AppState(rx.State):
             self.city = f"{response.json()['location']['name']}"
             self.country = f"{response.json()['location']['country']}"
             self.icon = f"{response.json()['current']['condition']['icon']}"
-            self.show = not (self.show) 
+            self.show = not (self.show)
         except Exception:
             ## Add code that will trigger opening a dialog component
             self.city = None
             self.country = None
             self.icon = None
-
             self.temp = "Error city not found try something else"
-
-            # Task 2 - Done
-            # Format into a nicely styled box component that displays
-            # Country name, city name, weather in celcius AND it should be placed
-            # inside a box component similar to https://i.imgur.com/dtKFDY1.png
-
             # Task 3
             # 1. Handle errors gracefully, if city was not found, display a custom dialog component that says `City not found try something else`. Read about try except in python
             # 2. When city is found clear the city from search input
             # 3. When city is not found, clear state set all variables to empty string
             # 4. Style and user new components (see screenshot in telegram for reference)
             # 5. Center align the components in the middle of the screen
+
     def clear_input_text(self):
         self.text = ""
 
+
 def index():
     return rx.vstack(
+        rx.hstack(
+            rx.text("ㅤㅤ"),
+            rx.badge(
+                "Weather Forecast by Daesun", variant="subtle", color_scheme="blue"
+            ),
+            rx.text("ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ"),
+            rx.link(
+                "GitHub",
+                href="https://github.com/daesun06/laconica",
+                color="rgb(107,99,246)",
+            ),
+            rx.text("ㅤㅤㅤ"),
+            bg="black",
+            color="white",
+            height="5vh",
+        ),
+        rx.box("", height="40vh", bg=""),
         rx.hstack(
             rx.input(on_change=AppState.set_text, value=AppState.text),
             rx.button(
@@ -59,7 +71,7 @@ def index():
                 "Clear",
                 color_scheme="blue",
                 on_click=AppState.clear_input_text,
-            )
+            ),
         ),
         rx.cond(
             AppState.show,
@@ -76,13 +88,18 @@ def index():
                         ),
                         bg="white",
                         border_radius="lg",
-                        width="80%"
+                        width="80%",
                     ),
                     rx.text(AppState.temp),
                 ),
             ),
-        )
+        ),
+        rx.box("", height="44.5vh", bg="#1D2330"),
+        center_content=True,
+        bg="#1D2330",
+        color="white",
     )
+
 
 app = rx.App(state=AppState)
 app.add_page(index)
